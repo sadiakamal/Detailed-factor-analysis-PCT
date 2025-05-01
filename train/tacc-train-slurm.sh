0e1c721e516ae6acce757b02a69da52a424e65be
Sagnik Ray Choudhury <sagnikrayc@gmail.com>
2025-05-01 16:13:03 -0500

    chore: slurm + gpu works perfectly (#9)

---
+++ train/tacc-train-slurm.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#----------------------------------------------------
+# Sample Slurm job script
+#   for TACC Lonestar6 AMD Milan nodes
+#
+#   *** Serial Job in Normal Queue***
+# 
+# Last revised: October 22, 2021
+#
+# Notes:
+#
+#  -- Copy/edit this script as desired.  Launch by executing
+#     "sbatch milan.serial.slurm" on a Lonestar6 login node.
+#
+#  -- Serial codes run on a single node (upper case N = 1).
+#       A serial code ignores the value of lower case n,
+#       but slurm needs a plausible value to schedule the job.
+#
+#  -- Use TACC's pylauncher utility to run multiple serial 
+#       executables at the same time, execute "module load pylauncher" 
+#       followed by "module help pylauncher".
+#----------------------------------------------------
+
+#SBATCH -J plb-train           # Job name
+#SBATCH -o slurm/%j.out       # Name of stdout output file
+#SBATCH -e slurm/%j.err       # Name of stderr error file
+#SBATCH -p gpu-a100-small  # Queue (partition) name
+#SBATCH -N 1               # Total # of nodes (must be 1 for serial)
+#SBATCH -n 1               # Total # of mpi tasks (should be 1 for serial)
+#SBATCH -t 01:30:00        # Run time (hh:mm:ss)
+#SBATCH --mail-type=all    # Send email at begin and end of job
+#SBATCH -A DBS24006 # Project/Allocation name (req'd if you have more than 1)
+#SBATCH --mail-user=sagnikraychoudhury@gmail.com
+
+# Any other commands must follow all #SBATCH directives...
+module load cuda/12.2
+${WORK}/anaconda3/envs/polbias/bin/python3.12 -c "import torch; print(f'cuda available: {torch.cuda.is_available()}')"
+date
+
+# Launch serial code...
+#./myprogram         # Do not use ibrun or any other MPI launcher
